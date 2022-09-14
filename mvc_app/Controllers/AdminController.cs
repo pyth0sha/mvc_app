7e8472a397405c6bd29d7a533346fab1accf95f7
Ivan Logvinov <ivanlogvinov@outlook.com>
2022-09-14 15:49:53 +0300

    Modified Department

---
+++ mvc_app/Controllers/AdminController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using mvc_app.Models;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace mvc_app.Controllers
+{
+    [Authorize(Roles ="admin")]
+    public class AdminController : Controller
+    {
+        private ApplicationContext db;
+        public AdminController(ApplicationContext context)
+        {
+            db = context;
+        }
+        public async Task<IActionResult> UserList()
+        {
+            ViewBag.Role = "Admin";
+            return View(await db.Users.ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(User user)
+        {
+            db.Users.Add(user);
+            await db.SaveChangesAsync();
+            return RedirectToAction("UserList");
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id != null)
+            {
+                User user = await db.Users.FirstOrDefaultAsync(p => p.Id == id);
+                ViewBag.Name = user.Number;
+                if (user != null)
+                    return View(user);
+            }
+            return NotFound();
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id != null)
+            {
+                User user = await db.Users.FirstOrDefaultAsync(p => p.Id == id);
+                if (user != null)
+                    return View(user);
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(User user)
+        {
+            db.Users.Update(user);
+            await db.SaveChangesAsync();
+            return RedirectToAction("UserList");
+        }
+
+        [HttpGet]
+        [ActionName("Delete")]
+        public async Task<IActionResult> ConfirmDelete(int? id)
+        {
+            if (id != null)
+            {
+                User user = await db.Users.FirstOrDefaultAsync(p => p.Id == id);
+                if (user != null)
+                    return View(user);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id != null)
+            {
+                User user = new User { Id = id.Value };
+                db.Entry(user).State = EntityState.Deleted;
+                await db.SaveChangesAsync();
+                return RedirectToAction("UserList");
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ mvc_app/Models/Department.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace mvc_app.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+    }
+}
