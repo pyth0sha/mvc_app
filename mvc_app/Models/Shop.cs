b6245911fce03aa6eefce8fbdf751ab4ab60ddc2
Ivan Logvinov <ivanlogvinov@outlook.com>
2022-09-15 12:32:12 +0300

    Added Shop model (part of Department)

---
+++ mvc_app/Models/Shop.cs
@@ -0,0 +1,11 @@
+﻿namespace mvc_app.Models
+{
+    //класс цеха
+    public class Shop
+    {
+        public int Id { get; set; }
+        public string Number { get; set; }
+        public int? DepartmentId { get; set; }
+        public Department Department { get; set; }
+    }
+}
