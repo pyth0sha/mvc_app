dd5fd2bd425f26eb2868f4296e4548f321b6885b
Ivan Logvinov <ivanlogvinov@outlook.com>
2022-10-21 16:36:34 +0300

    Returned actions for every shop

--- mvc_app/Controllers/ShopsController.cs
+++ mvc_app/Controllers/ShopsController.cs
@@ -10,7 +10,6 @@ using System.Linq;
 
 namespace mvc_app.Controllers
 {
-    //[Breadcrumb("Отдел")]
     [Authorize]
     public class ShopsController : Controller
     {
@@ -22,10 +21,16 @@ namespace mvc_app.Controllers
             db = context;
             _logger = logger;
         }
+        /// <summary>
+        /// Перенаправляет на нужную форму в зависимости от цеха
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="dataEntered"></param>
+        /// <returns></returns>
 
         [Route("Shops/Edit")]
         [Breadcrumb("Цех", FromAction="Index", FromController=typeof(HomeController))]
-        public async Task<IActionResult> Index(int? id, bool? dataEntered)
+        public async Task<IActionResult> Index(int? id)
         {   
             User user = await db.Users.FirstOrDefaultAsync(p => p.Number == User.Identity.Name);
             Role role = await db.Roles.FirstOrDefaultAsync(p => p.Id == user.RoleId);
@@ -40,81 +45,113 @@ namespace mvc_app.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Proceed(Data101 data101,
-                                            Data102 data102,
-                                            Data104 data104,
-                                            Data105 data105,
-                                            Data106_1 data106_1,
-                                            Data106_2 data106_2,
-                                            Data201 data201,
-                                            Data204 data204,
-                                            Data401 data401,
-                                            Data402 data402)
+        public async Task<IActionResult> Shop101(Data101 data)
         {
             User user = await db.Users.FirstOrDefaultAsync(p => p.Number == User.Identity.Name);
-            int? shopId = 0;
-            if(data101 != null){
-                data101.ShopId = 1;
-                shopId = data101.ShopId;
-                data101.Calculate();
-                db.data101.Add(data101);
-            }
-            else if(data102 != null){
-                data102.ShopId = 2;
-                shopId = data102.ShopId;
-                data102.Calculate();
-                db.data102.Add(data102);
-            }
-            else if(data104 != null){
-                data104.ShopId = 3;
-                shopId = data104.ShopId;
-                data104.Calculate();
-                db.data104.Add(data104);
-            }
-            else if(data105 != null){
-                data105.ShopId = 4;
-                shopId = data105.ShopId;
-                data105.Calculate();
-                db.data105.Add(data105);
-            }
-            else if(data106_1 != null){
-                data106_1.ShopId = 5;
-                shopId = data106_1.ShopId;
-                data106_1.Calculate();
-                db.data106_1.Add(data106_1);
-            }
-            else if(data106_2 != null){
-                data106_2.ShopId = 6;
-                shopId = data106_2.ShopId;
-                data106_2.Calculate();
-                db.data106_2.Add(data106_2);
-            }
-            else if(data201 != null){
-                data201.ShopId = 7;
-                //data201.Calculate();
-                db.data201.Add(data201);
-            }
-            else if(data204 != null){
-                data204.ShopId = 8;
-                shopId = data204.ShopId;
-                data204.Calculate();
-                db.data204.Add(data204);
-            }
-            else if(data401 != null){
-                data401.ShopId = 9;
-                shopId = data401.ShopId;
-                data401.Calculate();
-                db.data401.Add(data401);
-            }
-            else if(data402 != null){
-                data402.ShopId = 10;
-                shopId = data402.ShopId;
-                data402.Calculate();
-                db.data402.Add(data402);
-            }
-            
+            data.ShopId = user.ShopId;
+            data.Calculate();
+            db.data101.Add(data);
             await db.SaveChangesAsync();
-            return RedirectToAction("Index", new {id=shopId, dataEntered=true});
+            return RedirectToAction("Index", new {id=1});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Shop102(Data102 data)
+        {
+            User user = await db.Users.FirstOrDefaultAsync(p => p.Number == User.Identity.Name);
+            data.ShopId = user.ShopId;
+            data.Calculate();
+            db.data102.Add(data);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index", new {id=2});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Shop104(Data104 data)
+        {
+            User user = await db.Users.FirstOrDefaultAsync(p => p.Number == User.Identity.Name);
+            data.ShopId = user.ShopId;
+            data.Calculate();
+            db.data104.Add(data);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index", new {id=3});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Shop105(Data105 data)
+        {
+            User user = await db.Users.FirstOrDefaultAsync(p => p.Number == User.Identity.Name);
+            data.ShopId = user.ShopId;
+            data.Calculate();
+            db.data105.Add(data);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index", new {id=4});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Shop106_1(Data106_1 data)
+        {
+            User user = await db.Users.FirstOrDefaultAsync(p => p.Number == User.Identity.Name);
+            data.ShopId = user.ShopId;
+            data.Calculate();
+            db.data106_1.Add(data);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index", new {id=5});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Shop106_2(Data106_2 data)
+        {
+            User user = await db.Users.FirstOrDefaultAsync(p => p.Number == User.Identity.Name);
+            data.ShopId = user.ShopId;
+            data.Calculate();
+            db.data106_2.Add(data);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index", new {id=6});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Shop201(Data201 data)
+        {
+            // User user = await db.Users.FirstOrDefaultAsync(p => p.Number == User.Identity.Name);
+            // data.ShopId = user.ShopId;
+            // data.Calculate();
+            // db.data201.Add(data);
+            // await db.SaveChangesAsync();
+            return RedirectToAction("Index", new {id=7});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Shop204(Data204 data)
+        {
+            User user = await db.Users.FirstOrDefaultAsync(p => p.Number == User.Identity.Name);
+            data.ShopId = user.ShopId;
+            data.Calculate();
+            db.data204.Add(data);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index", new {id=8});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Shop401(Data401 data)
+        {
+            User user = await db.Users.FirstOrDefaultAsync(p => p.Number == User.Identity.Name);
+            data.ShopId = user.ShopId;
+            data.Calculate();
+            db.data401.Add(data);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index", new {id=9});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Shop402(Data402 data)
+        {
+            User user = await db.Users.FirstOrDefaultAsync(p => p.Number == User.Identity.Name);
+            data.ShopId = user.ShopId;
+            data.Calculate();
+            db.data402.Add(data);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index", new {id=10});
         }
 
         public async Task<IActionResult> ShopHistory(int? id, int? pageNumber)
